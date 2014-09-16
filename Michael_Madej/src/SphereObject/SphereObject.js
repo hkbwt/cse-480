1f54a94fc224bd8074ef641946e25cc3861b9b7d
Meekgal <mpmadej@oakland.edu>
2014-09-16 10:33:43 -0400

    Moved stuff around
    Added a sphere object and moved stuff around

---
+++ Michael_Madej/src/SphereObject/SphereObject.js
@@ -0,0 +1,11 @@
+//needs a webgl scene as a constructor
+ 
+SphereObject = function(scene){
+	var scene;
+	var radius;
+	var rings;
+	this.scene = scene;
+	
+}
+SphereObject.prototype = new NodeObject();
+SphereObject.prototype.testMethod = function(){alert("test");}

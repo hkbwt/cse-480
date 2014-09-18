445ce223f45e530b351f1ddc624e4846a86e2b25
Shawn Scott <sscott333@gmail.com>
2014-09-18 13:09:02 -0400

    updated factories and debug

--- libs/factories.js
+++ libs/factories.js
@@ -9,53 +9,53 @@
 *
 *********************************************************/
 
-var FACTORIES = {
+var Factories = {
+	Shapes: {
+		createSphere: function(radius, material) {
+			var sphereGeo = new THREE.SphereGeometry(radius, 20, 20);
+			var sphere = new THREE.Mesh(sphereGeo, material);
+			sphere.castShadow = true;
 
-	createSphere: function(radius, hexColor) {
-		if (hexColor == undefined) {
-			hexColor = 0xff0000;
+			return sphere;
+		},
+
+		createBox: function(width, height, depth, material) {
+			var cubeGeo = new THREE.BoxGeometry(width, height, depth);
+			var cube = new THREE.Mesh(cubeGeo, material);
+			cube.castShadow = true;
+			return cube;
+		},
+
+		createPlane: function(rect, material) {
+			var planeGeo = new THREE.PlaneGeometry(rect.x, rect.y);
+	     	var plane = new THREE.Mesh(planeGeo, material);
+	        plane.receiveShadow  = true;
+
+	        return plane
 		}
-		var sphereGeo = new THREE.SphereGeometry(radius, 20, 20);
-		var sphereMat = new THREE.MeshLambertMaterial({color: hexColor});
-		var sphere = new THREE.Mesh(sphereGeo, sphereMat);
-		sphere.castShadow = true;
 
-		return sphere;
 	},
 
-	createBox: function(width, height, depth, hexColor) {
-		if (hexColor == undefined) {
-			hexColor = 0xff0000;
+	Lights: {
+
+		createSpotLight: function(pos, hexColor) {
+			hexColor = ( hexColor != undefined ) ? hexColor : 0xffffff;
+			var spotLight = new THREE.SpotLight(hexColor);
+			spotLight.position.set( pos.x, pos.y, pos.z);
+	        spotLight.castShadow = true;
+			
+			return spotLight;
 		}
-		var cubeGeo = new THREE.BoxGeometry(width, height, depth);
-		var cubeMat = new THREE.MeshLambertMaterial({color: hexColor});
-		var cube = new THREE.Mesh(cubeGeo, cubeMat);
-		cube.castShadow = true;
-		return cube;
+
 	},
 
-	createPlane: function(rect, hexColor) {
-		if (hexColor == undefined) {
-			hexColor = 0xffffff;
-		}
-		var planeGeometry = new THREE.PlaneGeometry(rect.x, rect.y);
-        var planeMaterial = new THREE.MeshLambertMaterial({color: hexColor});
-        var plane = new THREE.Mesh(planeGeometry,planeMaterial);
-        plane.receiveShadow  = true;
+	Materials: {
+		createLambertMaterial: function(hexColor) {
+			hexColor = ( hexColor != undefined ) ? hexColor : 0xffffff;
+			return new THREE.MeshLambertMaterial({color: hexColor});
 
-        return plane
-	},
 
-	createSpotLight: function(pos, hexColor) {
-		if (hexColor == undefined) {
-			hexColor = 0xffffff;
 		}
-		var spotLight = new THREE.SpotLight(hexColor);
-		spotLight.position.set( pos.x, pos.y, pos.z);
-        spotLight.castShadow = true;
-		
-		return spotLight;
 	}
-
 
 }

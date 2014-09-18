5a860a5604a67d714689d3c6af71e24d7ff704a7
Shawn Scott <sscott333@gmail.com>
2014-09-18 09:13:49 -0400

    added detector updated engine

--- libs/engine.js
+++ libs/engine.js
@@ -9,70 +9,84 @@
 *
 *********************************************************/
 
-var ENGINE = {
+function Engine(domElement, bRenderStats) {
 
+		this.stats = undefined;
+		this.scene = undefined;
+		this.camera = undefined;
+		this.renderer = undefined;
+		this.ambientLight = undefined;
+		this.bStats = ( bRenderStats != undefined ) ? bRenderStats : true;
+		this.domElement = domElement;
 
-engine: function() {
+		this.init();
+}
 
-		function initStats(){
-			var stats = new Stats();
-			stats.setMode(0);
-			$("#Stats-output").append( stats.domElement );
-			return stats;
+Engine.prototype.init = function() {
+	if (Detector.webgl) {
+
+		if (this.bStats == true) {
+			this.stats = this.initStats();
 		}
 
-		function createScene(){
-			return new THREE.Scene();
-		}
-
-		function createCamera() {
-			return new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
-		}
-
-		function createRenderer() {
-			var renderer = new THREE.WebGLRenderer();
-			renderer.setClearColorHex(0xEEEEEE, 1.0);
-			renderer.setSize(window.innerWidth, window.innerHeight);
-			renderer.shadowMapEnabled = true;
-
-			$("#WebGL-output").append(renderer.domElement);
-			return renderer;
-		}
-
-		function createAmbientLight() {
-			return new THREE.AmbientLight(0x0c0c0c);
-		}
-
-
-		this.stats = initStats();
-
-		this.scene = createScene();
-		this.camera = createCamera();
-		this.renderer = createRenderer();
-		this.ambientLight = createAmbientLight();
-
-
-		this.render = ENGINE.render;
-		this.addAxes = ENGINE.addAxes;
+		this.scene = this.createScene();
+		this.camera = this.createCamera();
+		this.renderer = this.createRenderer();
+		this.ambientLight = this.createAmbientLight();
 
 		this.scene.add(this.ambientLight);
+	}
+	else {
+		Detector.addGetWebGLMessage();
+	}
+}
 
-	},
 
-
-	render: function() {
-		this.stats.update();
-		window.requestAnimationFrame(this.render.bind(this));
-	    this.renderer.render(this.scene, this.camera);
-
-	},
-
-	addAxes: function() {
-		var axes = new THREE.AxisHelper(20);
-		this.scene.add(axes);
+Engine.prototype.initStats = function() {
+	if(this.bStats == false){
+		this.bStats = true;
 	}
 
+	var stats = new Stats();
+	stats.setMode(0);
+	$("#Stats-output").append( stats.domElement );
+	return stats;
+}
+
+Engine.prototype.createScene = function() {
+	return new THREE.Scene();
+}
+
+Engine.prototype.createCamera = function() {
+	return new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
+}
+
+Engine.prototype.createRenderer = function() {
+	var renderer = new THREE.WebGLRenderer();
+	renderer.setClearColorHex(0xEEEEEE, 1.0);
+	renderer.setSize(window.innerWidth, window.innerHeight);
+	renderer.shadowMapEnabled = true;
+
+	$(this.domElement).append(renderer.domElement);
+	return renderer;
+}
+
+Engine.prototype.createAmbientLight =  function() {
+	return new THREE.AmbientLight(0x0c0c0c);
+}
 
 
+ Engine.prototype.render = function() {
+ 	if(this.bStats) {
+ 		this.stats.update();
+ 	}
+	
+	window.requestAnimationFrame(this.render.bind(this));
+    this.renderer.render(this.scene, this.camera);
 
+}
+
+Engine.prototype.addAxes = function() {
+	var axes = new THREE.AxisHelper(20);
+	this.scene.add(axes);
 }

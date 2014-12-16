05d6eb4fbd037697bd2ad75ab987607bae10b0cf
Shawn Scott <noteaparties@comcast.net>
2014-12-16 08:37:15 -0500

    added graphmodel debug

--- js/app/GraphModel.js
+++ js/app/GraphModel.js
@@ -9,6 +9,19 @@
 *********************************************************/
 
 /*Graph Model
+    
+    Babylon Materials and Colors
+    Pointer events; Pick events
+    Mimicing Graph Structure with model objects on screen
+
+    Things to do:
+    create materials
+    set a defualt color theme for 
+        vertex color
+        edge color
+        active or marked color
+        selected color
+
 */
 
 
@@ -26,20 +39,23 @@ var GraphStates = {
 };
 
 
-var GraphModel = function (scene, vertices, theme, size) {    
+var GraphModel = function (scene, theme, size) {    
 
     //initialize Graph ADT
     Graph.call(this);
 
     if(typeof(size) == "undefined") {
         size = 5.0;
+    }
+
+    if(typeof(theme) == "undefined") {
+        theme = {/*defualt theme*/};
     }  
 
-    this.scene          = scene;                              
-    this.theme          = theme;                            
-    this.meshSize       = size;                                
-    this.meshSegments   = 10.0;                                
-    this.circleRaidus   = 10.0;
+    this._scene         = scene;                              
+    this._theme         = theme;                            
+    this._meshSize      = size;                                
+    this._meshSegments  = 10.0;
     
     this._selectedQueue = [];
 };
